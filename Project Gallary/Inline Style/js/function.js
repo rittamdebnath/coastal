83d9be54f7dde66c432d10d12de4be3d653838c6
rittamdebnath <rittamdebnath@gmail.com>
2016-02-19 21:09:53 +0530

    update

---
+++ Project Gallary/Inline Style/js/function.js
@@ -0,0 +1,50 @@
+$(window).scroll(function() {
+
+  var wScroll = $(this).scrollTop();
+
+  $('.logo').css({
+    'transform': 'translate(0px, ' + wScroll / 2 + '%)'
+  });
+
+  $('.back-bird').css({
+    'transform': 'translate(0px, ' + wScroll / 4 + '%)'
+  });
+
+  $('.fore-bird').css({
+    'transform': 'translate(0px, -' + wScroll / 14 + '%)'
+  });
+
+  // 500 is the arbitary size or we can put $(window).height() / 1.2
+  if (wScroll > $('.clothes-pics').offset().top - $(window).height() / 1.2) {
+
+    $('.clothes-pics figure').each(function(i) {
+
+      setTimeout(function() {
+        $('.clothes-pics figure').eq(i).addClass('is-showing');
+
+      }, 150 * (i + 1));
+
+    });
+  }
+
+  if (wScroll > $('.large-window').offset().top - $(window).height()) {
+    $('.large-window').css({
+      'background-position': 'center ' + (wScroll - $('.large-window').offset().top) + 'px'
+    });
+    var opacity = (wScroll - $('.large-window').offset().top + 500) / (wScroll / 5)
+    $('.window-tint').css({
+      'opacity': opacity
+    });
+  }
+  if(wScroll > $('.blog-posts').offset().top - $(window).height()){
+
+    var offset = (Math.min(0, wScroll - $('.blog-posts').offset().top +$(window).height() - 350)).toFixed();
+
+    $('.post-1').css({'transform': 'translate('+ offset +'px, '+ Math.abs(offset * 0.2) +'px)'});
+
+    $('.post-3').css({'transform': 'translate('+ Math.abs(offset) +'px, '+ Math.abs(offset * 0.2) +'px)'});
+
+  }
+
+
+});
